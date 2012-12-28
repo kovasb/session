b7cbc55b632daa4e57fad826d98ef2f02c042245
Jonas Enlund <jonas.enlund@gmail.com>
2012-12-28 16:35:48 +0200

    Replaced noir and noir-async with compojure and aleph

--- src/clj/session/server.clj
+++ src/clj/session/server.clj
@@ -1,38 +1,53 @@
 (ns session.server
   (:require [session.datomic :as datomic]
             [session.views.common :as common]
-            [noir.core :as nc]
-            [noir-async.core :as nac]
+            [lamina.core :as lamina]
             [aleph.http :as http]
-            [noir.server :as server]))
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
+            [compojure.handler :refer [site]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]))
 
 
-(nc/defpage "/" []
-  (common/layout
-   (seq [[:div.navbar.navbar-fixed-top
-          [:div.navbar-inner
-           [:div.container {:style "margin-left:80px"}
-            [:span {:href "#" :style "font-size:20px;margin-left:0px"} "session"]
-            [:ul.nav]]]]
-         [:div.container {:style "margin-left:20px"}
-          [:div.content]]])))
+(defn service-handler [channel]
+  (datomic/subscribe-channel channel)
+  (lamina/receive-all channel #(datomic/service-request (read-string %))))
 
-(nc/defpage "/get_session" args
-  {:status 202
-   :headers {"Content-Type" "application/edn; charset=utf-8"}
-   :body (pr-str (datomic/get-datomic-session))})
+(defn service [channel request]
+  (if (:websocket request)
+    (service-handler channel)
+    (lamina/enqueue channel request)))
 
-(nac/defpage-async "/service" [] conn
-  (datomic/subscribe-channel (:request-channel conn))
-  (nac/on-receive conn #(datomic/service-request (read-string %))))
+(defroutes routes
 
+  (GET "/" _
+    (common/layout
+     (seq [[:div.navbar.navbar-fixed-top
+            [:div.navbar-inner
+             [:div.container {:style "margin-left:80px"}
+              [:span {:href "#" :style "font-size:20px;margin-left:0px"} "session"]
+              [:ul.nav]]]]
+           [:div.container {:style "margin-left:20px"}
+            [:div.content]]])))
+
+  (GET "/get_session" _
+    {:status 202
+     :headers{"Content-Type" "application/edn; charset=utf-8"}
+     :body (pr-str (datomic/get-datomic-session))})
+  
+  (GET "/service" _
+    (http/wrap-aleph-handler service))) 
 
 (defn -main [& m]
-  (let [mode :dev
-        port (Integer/parseInt (first m))
-        noir-handler (server/gen-handler {:mode mode})]
-    (session.datomic/setup  (last m))
+  (let [port (Integer/parseInt (first m))
+        duri (last m)]
+    (session.datomic/setup duri)
     (http/start-http-server
-     (http/wrap-ring-handler noir-handler)
+     (-> routes
+         site
+         wrap-file-info
+         (wrap-resource "public/")
+         http/wrap-ring-handler)
      {:port port :websocket true})
     (datomic/process-requests-thread @datomic/conn)))
