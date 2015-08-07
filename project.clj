f14f182f8dfa0c33345bee71d880df8f3b9e744b
kovasb <kovas.boguta@gmail.com>
2015-08-07 17:06:30 -0400

    remove dep

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.28"]
-                 [kovasb/gamma "0.0-135-9-g52d4**DIRTY**"]
                  [cljsjs/codemirror "5.1.0-2"]
                  [org.omcljs/om "0.9.0"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]])
